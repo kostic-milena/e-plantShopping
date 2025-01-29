ce95e9a27c28a40e75d9f8820518bafbf1975629
kostic-milena <anticz.milena@gmail.com>
2025-01-29 08:33:36 -0500

    update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -13,8 +13,6 @@ function ProductList(props) {
     const dispatch = useDispatch();
 
     const cartItems=useSelector(state => state.cart.items);
-    console.log(cartItems);
-    // setCart(cartItems);
     useEffect(() => {
         
     }, []);
